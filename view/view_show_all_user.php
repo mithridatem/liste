2c88c73272954a88567367fbc08691fe69af5915
mithridatem <72612782+mithridatem@users.noreply.github.com>
2022-05-19 00:43:34 +0200

    nettoyage repository

--- view/view_show_all_user.php
+++ view/view_show_all_user.php
@@ -4,7 +4,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./asset/style/style.css">
-    <title>Document</title>
+    <title>Liste utilisateurs</title>
 </head>
 <body>
     <!--Liste des utilisateurs-->
