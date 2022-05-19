e757b31cfd52c821570edbf4b575048004c9373c
mithridatem <72612782+mithridatem@users.noreply.github.com>
2022-05-19 02:26:08 +0200

    update

--- index.php
+++ index.php
@@ -13,29 +13,33 @@
     //boucle affichage de la liste des utilisateurs
     foreach($liste as $value){
         echo '<tr class="select">
-            <td>'.$value['name_user'].'</td>
-            <td>'.$value['first_name_user'].'</td>
-            <td>';
-            echo '<select>
+                    <td>'.$value['name_user'].'</td>
+                    <td>'.$value['first_name_user'].'</td>
+                    <td>
+                <select>
             <option value="">sélectionnez un role</option>';
             //boucle affichage du menu déroulant role
             foreach($roles as $value2){
                 echo '<option value="'.$value2['id_role'].'">
                 '.$value2['name_role'].'</option>';
             }
-            echo '</select>';
-            echo '</td>
-            <td>'.$value['name_role'].'</td>
-            <td><a href="./update_user.php?id='.$value['id_user'].'"><img src="./asset/image/edit.png" class="edit"></a></td>
+            echo '</select>
+                </td>
+                <td>'.$value['name_role'].'</td>
+                <td><a href="./update_user.php?id='.$value['id_user'].'"><img src="./asset/image/edit.png" class="edit"></a></td>
             </tr>';
     }
     echo '</table></div></form>
     <script src="./asset/script/script.js"></script><div id="error"></div></body>
     </html>';
+
+    //gestion des erreurs
+    //test si il y à une erreur
     if(isset($_GET['error'])){
         echo "<script>let error = document.querySelector('#error');
         error.innerHTML = 'Veuillez sélectionner une catégorie, clic dans la liste pour confirmer la sélection'</script>";
     }
+    //test si un utilisateur à été mis à jours
     if(isset($_GET['name']) AND isset($_GET['first'])){
         echo '<script>let error = document.querySelector("#error")
         error.innerHTML = "Utilisateur : '.$_GET['name'].' '.$_GET['first'].' a été mis à jour"</script>';
